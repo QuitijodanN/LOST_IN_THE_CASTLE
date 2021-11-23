18932e8c391ea12e21e530d522e5afde6976c969
QuitijodanN <al397126@uji.es>
2021-11-23 14:42:16 +0100

    creacion script animacion jordi

---
+++ LOST IN THE CASTLE/Assets/Scripts/MágicJordicontroller.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MágicJordicontroller : MonoBehaviour
+{
+
+    Rigidbody2D rigidbody2d;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        Vector2 position = transform.position;
+        position.x = position.x + 3.0f * horizontal * Time.deltaTime;
+
+        rigidbody2d.MovePosition(position);
+    }
+}
